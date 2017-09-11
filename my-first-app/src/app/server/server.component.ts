66ea34707ec0d8a3502bd27d426548b106d27785
Luana <lms7@cin.ufpe.br>
2017-09-11 16:49:50 -0300

    Add constructor and getColor method on server component

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -8,7 +8,15 @@ export class ServerComponent{
     serverId = 10;
     serverStatus: string = "offline";
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus(){
         return this.serverId;
+    }
+
+    getColor(){
+        return this.serverStatus  === 'online' ? 'green' : 'red';
     }
 }
