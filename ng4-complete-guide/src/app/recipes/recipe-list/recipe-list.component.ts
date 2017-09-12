6878b777f1ade9adb08f820fc2473f7259f1ab7c
Luana <lms7@cin.ufpe.br>
2017-09-12 10:32:19 -0300

    Add some recipes on list for testing

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a simple test', 'http://www.seriouseats.com/images/2015/09/20150914-pressure-cooker-recipes-roundup-09.jpg')
+  ];
 
   constructor() { }
 
